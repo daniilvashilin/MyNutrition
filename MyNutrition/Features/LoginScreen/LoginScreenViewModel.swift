831d87832e6dbb79d9a4fadf2d3c49bda505cc7a
Danil Vaschilin <114740409+daniilvashilin@users.noreply.github.com>
2024-12-17 16:51:25 +0200

    Worikin with firbase

--- MyNutrition/Features/LoginScreen/LoginScreenViewModel.swift
+++ MyNutrition/Features/LoginScreen/LoginScreenViewModel.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct LoginScreenViewModel: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
