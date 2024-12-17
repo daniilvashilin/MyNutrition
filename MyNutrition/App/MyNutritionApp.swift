4db0c75138b27ca788cc8ff5922c591f28680c43
Danil Vaschilin <114740409+daniilvashilin@users.noreply.github.com>
2024-12-17 13:52:27 +0200

    Initial commit

---
+++ MyNutrition/App/MyNutritionApp.swift
@@ -0,0 +1,27 @@
+//
+//  MyNutritionApp.swift
+//  MyNutrition
+//
+//  Created by Daniil on 17/12/2024.
+//
+
+import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct MyNutritionApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
